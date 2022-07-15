5b9f04b066dbff472580f89788c2628adf647c56
Bl3cter <blecster@gmail.com>
2022-07-15 21:54:37 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -32,7 +32,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return getAllUsers();
     }
 
     @Override
