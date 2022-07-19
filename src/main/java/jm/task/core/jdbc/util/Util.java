bc4fb6b3c15b7e8e0baa16c65f12b2ee475daa0a
Bl3cter <blecster@gmail.com>
2022-07-19 16:49:39 +0300

    ex2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -29,7 +29,7 @@ public class Util {
                 properties.put(Environment.URL, HOST);
                 properties.put(Environment.USER, LOGIN);
                 properties.put(Environment.PASS, PASSWORD);
-                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
                 properties.put(Environment.SHOW_SQL, "true");
                 properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
