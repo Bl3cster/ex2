d3aa709998035eca2c70ab2e552f956992d7613e
Bl3cter <blecster@gmail.com>
2022-07-19 17:41:05 +0300

    ex2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,56 +1,48 @@
 package jm.task.core.jdbc.util;
 
-import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Util {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String HOST = "jdbc:mysql://localhost:3306/connectiontest";
+    private static final String HOST = "jdbc:mysql://localhost:3306/test?serverTimezone=Europe/Moscow";
     private static final String LOGIN = "root";
     private static final String PASSWORD = "Blecster";
 
-    private static SessionFactory sessionFactory;
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
-                Properties properties = new Properties();
-
-                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                properties.put(Environment.URL, HOST);
-                properties.put(Environment.USER, LOGIN);
-                properties.put(Environment.PASS, PASSWORD);
-                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-                properties.put(Environment.SHOW_SQL, "true");
-                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-
-                configuration.setProperties(properties).addAnnotatedClass(User.class);
-
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-                System.out.println("Connection is OK");
-            } catch (Exception e) {
-                System.out.println("Connection is EMPTY, try again!");
-            }
-        }
-        return sessionFactory;
+    public static SessionFactory getSessionFactory(){
+        return createSessionFactory();
     }
 
+    private static SessionFactory createSessionFactory(){
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(getSettings()).build();
+        MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+        Metadata metadata = metadataSources.buildMetadata();
+        return metadata.getSessionFactoryBuilder().build();
 
-    public static void closeSessionFactory() {
-        sessionFactory.close();
     }
 
+    private static Map<String, String> getSettings() {
+        Map<String, String> settings = new HashMap<>();
+        settings.put("connection.driver_class", "com.mysql.cj.jdbc.Driver");
+        settings.put("dialect", "org.hibernate.dialect.MySQL8Dialect");
+        settings.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/test?serverTimezone=Europe/Moscow");
+        settings.put("hibernate.connection.username", "root");
+        settings.put("hibernate.connection.password", "Blecster");
+        settings.put("hibernate.current_session_context_class", "thread");
+        settings.put("hibernate.show_sql", "true");
+        settings.put("hibernate.format_sql", "true");
+        return settings;
+    }
 
     public static Connection getConnection() {
         Connection connection = null;
