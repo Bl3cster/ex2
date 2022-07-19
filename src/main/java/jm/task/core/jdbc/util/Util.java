2f31a65a77097d6add6e1471bc150e874ec3dab9
Bl3cter <blecster@gmail.com>
2022-07-19 16:24:59 +0300

    ex2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,15 +7,12 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/connectiontest";
-    private static final String USER = "root";
+    private static final String HOST = "jdbc:mysql://localhost:3306/connectiontest";
+    private static final String LOGIN = "root";
     private static final String PASSWORD = "Blecster";
 
     private static SessionFactory sessionFactory;
@@ -26,8 +23,8 @@ public class Util {
                 Properties properties = new Properties();
 
                 properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                properties.put(Environment.URL, URL);
-                properties.put(Environment.USER, USER);
+                properties.put(Environment.URL, HOST);
+                properties.put(Environment.USER, LOGIN);
                 properties.put(Environment.PASS, PASSWORD);
                 properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 properties.put(Environment.SHOW_SQL, "true");
@@ -49,17 +46,5 @@ public class Util {
 
     public static void closeSessionFactory() {
         sessionFactory.close();
-    }
-
-
-    public static Connection getConnection() {
-        Connection connection = null;
-        try {
-            Class.forName(DRIVER);
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return connection;
     }
 }
